1d39855f08d335b4dd655245aee13afb40a50329
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-04-30 22:38:01 -0600

    Extract the server object in a single file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-const express = require('express');
-
-const app = express();
+const app = require('./server');
 
 app.get('/', (req, res) =>
   res.json({
--- tests/example-api.test.js
+++ tests/example-api.test.js
@@ -1,7 +1,5 @@
 const request = require('supertest');
-const express = require('express');
-
-const app = express();
+const app = require('../src/server');
 
 app.get('/user', (req, res) => {
   res.status(200).json({ name: 'vincent' });
