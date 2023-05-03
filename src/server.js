c85ebd6958bac62b3afa364d3bb15966c0baa140
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-05-02 20:08:16 -0600

    Configure body-parser

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = require('./v1/routes/index');
 
 const app = express();
+
+// Body parser config
+// urlencoded: Analyse sended date through the codification application/x-www-form-urlencoded
+// urlencoded.extended: If false, uses querystring, otherwise, it uses qs
+// json: Analyse data sended by using JSON format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
 
 // Routes SetUp
 app.use('/v1', router);
