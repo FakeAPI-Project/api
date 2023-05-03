9d381b863e133c0281dfa14231833a8d0871b481
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-05-02 21:53:18 -0600

    Configure Sequelize and SQlite3

---
+++ src/config/database.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  development: {
+    username: 'admin',
+    password: 'admin',
+    database: 'fakeapi',
+    storage: path.join(path.resolve(__dirname), '../../db/database.db'),
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'sqlite',
+  },
+  test: {
+    username: 'admin',
+    password: 'admin',
+    database: 'fakeapi',
+    storage: path.join(path.resolve(__dirname), '../../db/database.db'),
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'sqlite',
+  },
+  production: {
+    username: 'admin',
+    password: 'admin',
+    database: 'fakeapi',
+    storage: path.join(path.resolve(__dirname), '../../db/database.db'),
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'sqlite',
+  }
+};
