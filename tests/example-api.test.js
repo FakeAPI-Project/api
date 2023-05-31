44c4871f3bbd387e1b61c7a2e6d27e3bd744ffc7
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-05-30 22:24:42 -0600

    Fix example test

--- tests/example-api.test.js
+++ tests/example-api.test.js
@@ -1,16 +1,16 @@
 const request = require('supertest');
 const app = require('../src/server');
 
-app.get('/user', (req, res) => {
-  res.status(200).json({ name: 'vincent' });
+app.get('/example', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' });
 });
 
 test('responds with json object with a name property', async () => {
   const response = await request(app)
-    .get('/user')
+    .get('/example')
     .set('Accept', 'application/json');
 
   expect(response.headers['content-type']).toMatch(/json/);
   expect(response.status).toEqual(200);
-  expect(response.body.name).toEqual('vincent');
+  expect(response.body.message).toEqual('Hello World!');
 });
