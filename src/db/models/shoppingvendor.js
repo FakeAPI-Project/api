60286a6fec10922de6101816165bc835900d8fb7
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-05-30 21:50:36 -0600

    Add migration to create vendors table

---
+++ src/db/models/shoppingvendor.js
@@ -0,0 +1,26 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class ShoppingVendor extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // eslint-disable-next-line
+    static associate(models) {
+      // define association here
+    }
+  }
+  ShoppingVendor.init({
+    name: DataTypes.STRING,
+    contactEmail: DataTypes.STRING,
+    phoneNumber: DataTypes.STRING,
+    isActive: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'ShoppingVendor',
+  });
+
+  return ShoppingVendor;
+};
