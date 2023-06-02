c3cebff8a78374730d7575490b55f48c7d5ad962
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-06-02 06:58:24 -0600

    Add seeder to populate categories

---
+++ src/db/seeders/20230602125426-shopping-categories.js
@@ -0,0 +1,39 @@
+const { faker } = require('@faker-js/faker');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  // eslint-disable-next-line
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    const categories = [];
+
+    for (let i = 0; i < 20; i++) {
+      categories.push({
+        name: faker.commerce.department(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      });
+    }
+
+    return queryInterface.bulkInsert('shopping_categories', categories);
+  },
+
+  // eslint-disable-next-line
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    return queryInterface.bulkDelete('shopping_categories', null, {});
+  }
+};
