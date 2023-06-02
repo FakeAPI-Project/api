22c7dcce40cd0ac414a60e6c88fd2321032fd60f
Vicente Gutiérrez <gtz.rvera@gmail.com>
2023-06-02 06:54:00 -0600

    Add new model for shopping categories

---
+++ src/db/models/ShoppingCategory.js
@@ -0,0 +1,23 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class ShoppingCategories extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    // eslint-disable-next-line
+    static associate(models) {
+      // define association here
+    }
+  }
+  ShoppingCategories.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'ShoppingCategory',
+    tableName: 'shopping_vendors',
+  });
+  return ShoppingCategories;
+};
